6925132e584f7c4502396ed024d197092fd38b9b
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-09-16 18:30:27 +0200

    Update fingertip_sensors_node.cpp

--- src/fingertip_sensors_node.cpp
+++ src/fingertip_sensors_node.cpp
@@ -18,7 +18,6 @@ public:
     int millisecondsTimer_;
     std::string serial_port_;
     int baudrate_;
-
     uint32_t serial_timeout_ = 1000;
     bool timestamp = false;
 
@@ -31,30 +30,22 @@ public:
     std::vector<uint16_t> ids;
     geometry_msgs::msg::Vector3 vec;
 
+    std::string sensor_state_topic_;
+
     rclcpp::Publisher<uclv_seed_robotics_ros_interfaces::msg::FTS3Sensors>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    void setSerialPortLowLatency(const std::string &serial_port)
-    {
-        std::cout << "Setting low latency for " << WARN_COLOR << serial_port << CRESET << std::endl;
-        std::string command = "setserial " + serial_port + " low_latency";
-        int result = system(command.c_str());
-        std::cout << "Setted low latency for " << WARN_COLOR << serial_port << CRESET
-                  << " result: " << SUCCESS_COLOR << result << CRESET << std::endl;
-    }
-
     FingertipSensors()
-        : Node("test_read_from_sensors")
+        : Node("fingertip_sensors"),
+          millisecondsTimer_(this->declare_parameter<int>("millisecondsTimer", 2)),
+          serial_port_(this->declare_parameter<std::string>("serial_port", "/dev/ttyUSB1")),
+          baudrate_(this->declare_parameter<int>("baudrate", 1000000)),
+          sensor_state_topic_(this->declare_parameter<std::string>("sensor_state_topic", "sensor_state"))
     {
-
-        millisecondsTimer_ = this->declare_parameter<int>("millisecondsTimer", 2);
-        serial_port_ = this->declare_parameter<std::string>("serial_port", "/dev/ttyUSB1");
-        baudrate_ = this->declare_parameter<int>("baudrate", 1000000);
-
         setSerialPortLowLatency(serial_port_);
         sensor_read_ = std::make_shared<serial::Serial>(serial_port_, baudrate_, serial::Timeout::simpleTimeout(serial_timeout_));
 
-        publisher_ = this->create_publisher<uclv_seed_robotics_ros_interfaces::msg::FTS3Sensors>("sensor_state", 1);
+        publisher_ = this->create_publisher<uclv_seed_robotics_ros_interfaces::msg::FTS3Sensors>(sensor_state_topic_, 1);
 
         sensor_read_->flush();
         write_on_serial("pausedata\r\n");
@@ -69,6 +60,15 @@ public:
     }
 
 private:
+    void setSerialPortLowLatency(const std::string &serial_port)
+    {
+        std::cout << "Setting low latency for " << WARN_COLOR << serial_port << CRESET << std::endl;
+        std::string command = "setserial " + serial_port + " low_latency";
+        int result = system(command.c_str());
+        std::cout << "Set low latency for " << WARN_COLOR << serial_port << CRESET
+                  << " result: " << SUCCESS_COLOR << result << CRESET << std::endl;
+    }
+
     void write_on_serial(const std::string &cmd)
     {
         sensor_read_->write(cmd);
@@ -77,52 +77,50 @@ private:
 
     void publish_state()
     {
-
         auto message = uclv_seed_robotics_ros_interfaces::msg::FTS3Sensors();
         message.header.stamp = rclcpp::Clock{}.now();
 
-        line = (sensor_read_->readline());
-        if (line == "")
+        line = sensor_read_->readline();
+        if (line.empty())
         {
-            // ERROR
+            RCLCPP_WARN(this->get_logger(), "No data received from the sensor.");
+            return;
         }
-        else
+
+        std::string token;
+        std::vector<std::string> tokens;
+        char delimiter = ',';
+        std::stringstream ss(line);
+
+        while (getline(ss, token, delimiter))
         {
-            std::string token;
-            std::vector<std::string> tokens;
-            char delimiter = ',';
-            std::stringstream ss(line);
+            tokens.push_back(token);
+        }
+        tokens.pop_back();
 
-            while (getline(ss, token, delimiter))
+        if (tokens.size() == 18)
+        {
+            timestamp = true;
+            var_timestamp = 2;
+        }
+
+        message.ids.resize(num_sensors);
+        ids.resize(num_sensors);
+
+        if (tokens[0] == "@")
+        {
+            for (size_t i = 1; i < 6; i++)
             {
-                tokens.push_back(token);
+                auto fx = tokens[var_timestamp - 2 + 3 * i];
+                auto fy = tokens[var_timestamp - 1 + 3 * i];
+                auto fz = tokens[var_timestamp + 0 + 3 * i];
+                ids[i] = i;
+                vec.x = std::stof(fx);
+                vec.y = std::stof(fy);
+                vec.z = std::stof(fz);
+                message.forces.push_back(vec);
             }
-            tokens.pop_back();
-
-            if (tokens.size() == 18)
-            {
-                timestamp = true;
-                var_timestamp = 2;
-            }
-
-            message.ids.resize(num_sensors);
-            ids.resize(num_sensors);
-
-            if (tokens[0] == "@")
-            {
-                for (size_t i = 1; i < 6; i++)
-                {
-                    auto fx = tokens[var_timestamp - 2 + 3 * i];
-                    auto fy = tokens[var_timestamp - 1 + 3 * i];
-                    auto fz = tokens[var_timestamp + 0 + 3 * i];
-                    ids[i] = i;
-                    vec.x = std::stof(fx);
-                    vec.y = std::stof(fy);
-                    vec.z = std::stof(fz);
-                    message.forces.push_back(vec);
-                }
-                message.ids = ids;
-            }
+            message.ids = ids;
         }
 
         publisher_->publish(message);
@@ -135,8 +133,8 @@ int main(int argc, char *argv[])
 
     try
     {
-        auto hand_driver_node = std::make_shared<FingertipSensors>();
-        rclcpp::spin(hand_driver_node);
+        auto fingertip_sensors_node = std::make_shared<FingertipSensors>();
+        rclcpp::spin(fingertip_sensors_node);
     }
     catch (const std::exception &e)
     {
