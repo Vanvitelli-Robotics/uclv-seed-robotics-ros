b5bea61f6846482656e9b5ab8aa5aa23d8e42cd1
Roberto Chello <36032212+robertochello@users.noreply.github.com>
2024-09-16 19:21:32 +0200

    Update bringup_hand.launch.py

--- launch/bringup_hand.launch.py
+++ launch/bringup_hand.launch.py
@@ -4,10 +4,10 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # Parametri comuni tra i nodi
+    # Common parameters for both nodes
     common_params = {
-        "baudrate": 1000000,  # Baudrate
-        "millisecondsTimer": 2  # Timer per la pubblicazione
+        "baudrate": 1000000,  # Baud rate for serial communication
+        "millisecondsTimer": 2  # Timer interval for publishing
     }
 
     return LaunchDescription([
@@ -17,13 +17,13 @@ def generate_launch_description():
             executable='hand_driver',
             name='hand_driver',
             parameters=[
-                common_params,  # Parametri comuni
-                {"serial_port": "/dev/ttyUSB0"},
-                {"motor_ids": [31, 32, 33, 34, 35, 36, 37, 38]},  # ID dei motori
-                {"motor_thresholds": [100, 3995]},  # Soglie dei motori
-                {"protocol_version": 2.0},  # Versione del protocollo
-                {"motor_state_topic": "motor_state"},  # Nome del topic di stato motori
-                {"desired_position_topic": "desired_position"}  # Nome del topic di posizioni desiderate
+                common_params,  # Common parameters
+                {"serial_port": "/dev/ttyUSB0"},  # Serial port for hand driver
+                {"motor_ids": [31, 32, 33, 34, 35, 36, 37, 38]},  # Motor IDs for the left hand
+                {"motor_thresholds": [100, 3995]},  # Threshold values for the motors
+                {"protocol_version": 2.0},  # Communication protocol version
+                {"motor_state_topic": "motor_state"},  # Topic name for motor state
+                {"desired_position_topic": "desired_position"}  # Topic name for desired motor positions
             ]
         ),
         Node(
@@ -32,9 +32,9 @@ def generate_launch_description():
             executable='fingertip_sensors',
             name='fingertip_sensors',
             parameters=[
-                common_params,
-                {"serial_port": "/dev/ttyUSB1"},  # 
-                {"sensor_state_topic": "sensor_state"}  # Nome del topic per lo stato dei sensori
+                common_params,  # Common parameters
+                {"serial_port": "/dev/ttyUSB1"},  # Serial port for the fingertip sensors
+                {"sensor_state_topic": "sensor_state"}  # Topic name for sensor state
             ]
         ),
     ])
